9786192a373242bf250aa0d40a9bcd308cc800de
annyhira414 <annyhira@gmail.com>
2024-06-28 03:32:32 +0600

    fixed some issue

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -1,7 +1,7 @@
-import { Dangerous, MoreVert, ReportProblem } from '@mui/icons-material';
-import { Box, IconButton, Menu, MenuItem } from '@mui/material';
-import React, { useState } from 'react';
-import { displayFormattedDate } from './baseUI/utils';
+import { Dangerous, MoreVert, ReportProblem } from "@mui/icons-material";
+import { Box, IconButton, Menu, MenuItem } from "@mui/material";
+import React, { useState } from "react";
+import { displayFormattedDate } from "./baseUI/utils";
 
 const TodoItem = ({ todo, moveTodo, onDelete, onEdit }) => {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -21,51 +21,39 @@ const TodoItem = ({ todo, moveTodo, onDelete, onEdit }) => {
 
   return (
     <div
-      className='todo-item'
+      className="todo-item"
       style={{
-        display: 'flex',
-        justifyContent: 'space-between',
+        display: "flex",
+        justifyContent: "space-between",
       }}
     >
       <Box>
         <Box
           sx={{
-            textAlign: 'center',
-            fontSize: '1.4rem',
-            marginBottom: '4px',
+            textAlign: "center",
+            fontSize: "1.4rem",
+            marginBottom: "4px",
           }}
         >
           {todo?.title}
         </Box>
         <Box>{todo?.description}</Box>
 
-        <Box mt={1}>
-          {' '}
-          Deadline: {`${todo?.date.startDate} - ${todo?.date.endDate}`}
-        </Box>
+        <Box mt={1}> Deadline: {`${todo?.date?.startDate} - ${todo?.date?.endDate}`}</Box>
         <Box mt={1}>
           <b
             style={{
-              color:
-                todo?.status === 'new'
-                  ? 'blue'
-                  : todo?.status === 'ongoing'
-                  ? '#f8a604'
-                  : '#44981a',
+              color: todo?.status === "new" ? "blue" : todo?.status === "ongoing" ? "#f8a604" : "#44981a",
             }}
           >
-            {todo?.status === 'new'
-              ? 'New'
-              : todo?.status === 'ongoing'
-              ? 'Ongoing'
-              : 'Done'}
+            {todo?.status === "new" ? "New" : todo?.status === "ongoing" ? "Ongoing" : "Done"}
           </b>
-          {todo?.status === 'ongoing' && (
+          {todo?.status === "ongoing" && (
             <Box>
               {taskChecker(todo?.date?.endDate) ? (
-                <Box mt={1}>
-                  {' '}
-                  <ReportProblem color='warning' />
+                <Box mt={1} display="flex">
+                  {" "}
+                  <ReportProblem color="warning" sx={{ fontSize: 16 }} />
                   The task is overdue!
                 </Box>
               ) : (
@@ -76,39 +64,23 @@ const TodoItem = ({ todo, moveTodo, onDelete, onEdit }) => {
         </Box>
       </Box>
       <Box>
-        <IconButton onClick={(event) => handleClick(event)} size='small'>
+        <IconButton onClick={(event) => handleClick(event)} size="small">
           <MoreVert />
         </IconButton>
-        {todo?.status === 'new' ? (
-          <Menu
-            anchorEl={anchorEl}
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={() => moveTodo(todo?.id, 'ongoing')}>
-              Ongoing
-            </MenuItem>
-            <MenuItem onClick={() => moveTodo(todo?.id, 'done')}>Done</MenuItem>
+        {todo?.status === "new" ? (
+          <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+            <MenuItem onClick={() => moveTodo(todo?.id, "ongoing")}>Ongoing</MenuItem>
+            <MenuItem onClick={() => moveTodo(todo?.id, "done")}>Done</MenuItem>
           </Menu>
-        ) : todo?.status === 'ongoing' ? (
-          <Menu
-            anchorEl={anchorEl}
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={() => moveTodo(todo?.id, 'new')}>New</MenuItem>
-            <MenuItem onClick={() => moveTodo(todo?.id, 'done')}>Done</MenuItem>
+        ) : todo?.status === "ongoing" ? (
+          <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+            <MenuItem onClick={() => moveTodo(todo?.id, "new")}>New</MenuItem>
+            <MenuItem onClick={() => moveTodo(todo?.id, "done")}>Done</MenuItem>
           </Menu>
         ) : (
-          <Menu
-            anchorEl={anchorEl}
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={() => moveTodo(todo?.id, 'new')}>New</MenuItem>
-            <MenuItem onClick={() => moveTodo(todo?.id, 'ongoing')}>
-              Ongoing
-            </MenuItem>
+          <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+            <MenuItem onClick={() => moveTodo(todo?.id, "new")}>New</MenuItem>
+            <MenuItem onClick={() => moveTodo(todo?.id, "ongoing")}>Ongoing</MenuItem>
           </Menu>
         )}
       </Box>
